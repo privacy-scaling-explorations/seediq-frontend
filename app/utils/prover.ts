ff2da142f9627f12c1ca09d15a66439715bde58c
0xvikasrushi <0xvikas@gmail.com>
2025-04-17 22:55:17 +0530

    feat: mutiple ipfs gateway

--- app/utils/prover.ts
+++ app/utils/prover.ts
@@ -1,11 +1,40 @@
 import { CIRCUIT_ASSETS, JWTCircuitInput } from "./constant";
 import * as snarkjs from "snarkjs";
 
+const IPFS_GATEWAYS = [
+  "https://gateway.pinata.cloud/ipfs/",
+  "https://4everland.io/ipfs/",
+  "https://w3s.link/ipfs/",
+  "https://dweb.link/ipfs/",
+  "https://trustless-gateway.link/ipfs/",
+  "https://nftstorage.link/ipfs/",
+];
+
 export async function fetchBinary(path: string): Promise<ArrayBuffer> {
-  const res = await fetch(path);
-  if (!res.ok) throw new Error(`Failed to fetch ${path}`);
-  return await res.arrayBuffer();
+  // If path is NOT an IPFS hash, just fetch normally
+  if (!path.startsWith("bafybe")) {
+    const res = await fetch(path);
+    if (!res.ok) throw new Error(`Failed to fetch ${path}`);
+    return await res.arrayBuffer();
+  }
+
+  // Otherwise, try fetching from multiple IPFS gateways
+  for (const gateway of IPFS_GATEWAYS) {
+    const url = `${gateway}${path}`;
+    try {
+      const res = await fetch(url);
+      if (res.ok) {
+        return await res.arrayBuffer();
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+      console.warn(`Fetch failed from ${url}, trying next...`);
+    }
+  }
+
+  throw new Error(`Failed to fetch ${path} from all gateways`);
 }
+
 export class JwtProver {
   static async generateProof(input: JWTCircuitInput) {
     try {
